7a25d0f0554ba6c91694e6e69b8f3afa1f9c8ca3
Natalia <rng2010@mail.ru>
2022-09-08 22:39:48 +0400

    домашнее задание к 3 семинару

--- dz3/task_05.py
+++ dz3/task_05.py
@@ -20,3 +20,28 @@ print(fibonacci(n))
 
 
 
+#def fib(n):
+#   if n in [1, 2]:                       
+#       return 1
+#   else:
+#       return fib(n-1) + fib(n-2)
+
+#def otrfib(n):
+#   if n == 1:                       
+#       return 1
+#   elif n == 2:                       
+#       return -1
+#  else:
+#        a, b = 1, -1
+#        for i in range(2, n):
+#            a, b = b, a - b
+#       return b
+
+#list = []
+#n = int(input('Введите число: '))
+#for x in range(1, n + 1):
+#    list.append(fib(x))
+#    list.insert(0, otrfib(x))
+#print(list)
+
+
