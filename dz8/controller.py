781d5b1ae38ebe630b1fb2165b4c5d4423ed5724
Natalia <rng2010@mail.ru>
2022-09-26 21:58:19 +0400

    домашнее задание к 8 семинару

---
+++ dz8/controller.py
@@ -0,0 +1,8 @@
+from sort import sort,get
+from vvod import get_number,getd
+from rabotafile import read_file,write_file
+from funczional import sortd
+
+
+def button():
+    return sortd(get_number,read_file,write_file,sort,get,getd)
